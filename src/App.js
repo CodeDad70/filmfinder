108c527b6f9d26e86bfada4b02d64cc0251012a4
CodeDad <github email address>
2018-08-13 14:52:41 -0400

    created folders for containers, components and actions

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class App extends Component {
-  render() {
-      return (
-      <div className="App">
-        <h1>Test</h1>
-      </div>
-      );
+
+class App extends React.Component {
+  render() { 
+    return (
+
+   
+  <div className="card-1">
+    <h1> Welcome to the FilmFinder</h1>
+    
+    
+  </div>
+    )
   }
-  }
-export default App;;
+}
+
+
+export default App;
