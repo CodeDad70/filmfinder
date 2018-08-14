3f3099085ea2e01da54b5b0e1303f0ba1ed1831b
CodeDad <github email address>
2018-08-13 23:46:51 -0400

    fixed poster_path to display posters with movie results

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,8 +1,11 @@
 import React from 'react';
-
+let imageUrl
 const MovieCard  = ({movie}) => (
+  
   <div key={movie.id} className = "MovieCard">
+  
     <div className = "card card-1">
+      <img className="AlbumImage" src = {"http://image.tmdb.org/t/p/w185//" + movie.poster_path} alt={movie.name} / >  
       <h3> {movie.title} </h3>
       
       
